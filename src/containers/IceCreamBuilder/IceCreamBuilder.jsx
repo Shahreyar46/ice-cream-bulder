899b0631832cd6f3e52c4c220f6e6eb391ced9e9
shahreyar46 <shahreyar.shimul15@gmail.com>
2022-06-30 00:59:54 +0600

    Version 1.0

---
+++ src/containers/IceCreamBuilder/IceCreamBuilder.jsx
@@ -0,0 +1,61 @@
+import { Component } from 'react';
+import Builder from '../../components/Builder/Builder';
+import IceCream from '../../components/IceCream/IceCream';
+import classes from './IceCreamBuilder.module.css';
+
+export default class IceCreamBuilder extends Component {
+    state = {
+        items: {
+            vanilla: 45,
+            chocolate: 34,
+            lemon: 44,
+            orange: 30,
+            strawberry: 60,
+        },
+        scoops: [],
+        totalPrice: 0,
+    };
+
+    addScoop = (scoop) => {
+        const { scoops, items } = this.state;
+        const workingScoops = [...scoops];
+        workingScoops.push(scoop);
+        this.setState((prevState) => {
+            return {
+                scoops: workingScoops,
+                totalPrice: prevState.totalPrice + items[scoop],
+            };
+        });
+    };
+
+    removeScoop = (scoop) => {
+        const { scoops, items } = this.state;
+        const workingScoops = [...scoops];
+
+        const scoopIndex = workingScoops.findIndex((sc) => sc === scoop);
+
+        workingScoops.splice(scoopIndex, 1);
+        this.setState((prevState) => {
+            return {
+                scoops: workingScoops,
+                totalPrice: prevState.totalPrice - items[scoop],
+            };
+        });
+    };
+
+    render() {
+        const { items, totalPrice, scoops } = this.state;
+        return (
+            <div className={['container', classes.container].join(' ')}>
+                <IceCream scoops={scoops} />
+                <Builder
+                    items={items}
+                    price={totalPrice}
+                    add={this.addScoop}
+                    remove={this.removeScoop}
+                    scoops={scoops}
+                />
+            </div>
+        );
+    }
+}
